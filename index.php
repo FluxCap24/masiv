f89e1e70b9169c95e6c5b719783a13574a2ebc10
fluxcap24 <vitaliy.katkov.2017@mail.ru>
2023-02-16 13:47:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>БД</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+$BD = array(
+    'user1' => array(
+        'id_user' => 1,
+        'firstname' => 'Иванов',
+        'name' => 'Иван',
+        'lastname' => 'Иван',
+        'login' => '1123',
+        'age' => '18',
+    ),
+    foreach ($BD as $BD => $items)
+
+   'user2' => array(
+
+   )
+
+);
+
+?>
